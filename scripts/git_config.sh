a6e9f624c8a8344d61a47ff81da2ecf451e7718d
Eth-H <ethull@protonmail.com>
2021-07-31 17:37:13 +0100

    rm macos specific config

--- scripts/git_config.sh
+++ scripts/git_config.sh
@@ -8,4 +8,3 @@ sudo apt-get install libsecret-1-0 libsecret-1-dev
 cd /usr/share/doc/git/contrib/credential/libsecret
 sudo make
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
-#git config --global credential.helper osxkeychain
